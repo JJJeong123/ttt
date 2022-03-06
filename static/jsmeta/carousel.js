65c6e2401521e5162c34a7bdfd6e6beb50b27c16
jin800812 <jin800812@gmail.com>
2022-03-06 18:03:34 +0900

    Fix absolute position of icons

--- static/jsmeta/carousel.js
+++ static/jsmeta/carousel.js
@@ -1,85 +1,45 @@
 function scrollL() {
   let x = document.getElementsByClassName("items")[0];
-  let step = window.outerWidth / 2;
+  let item = document.getElementsByClassName("item")[0];
+  let step = item.offsetWidth * 4 + 4;
   x.scrollLeft -= step;
 }
 
 function scrollR() {
   let x = document.getElementsByClassName("items")[0];
-  let step = window.outerWidth / 2;
+  let item = document.getElementsByClassName("item")[0];
+  let step = item.offsetWidth * 4 + 4;
   x.scrollLeft += step;
 }
 
 function getScrollVal() {
   setTimeout(() => {
+    let p = document.getElementById("carousel");
+    let style = p.currentStyle || window.getComputedStyle(p);
+
     let x = document.getElementsByClassName("items")[0];
     let el = document.getElementsByClassName("left_arrow")[0];
+  
     if (x.scrollLeft == 0) {
       el.style.display = "none";
-    } else {
+    } 
+    else {
       el.style.display = "flex";
+      el.style.left = parseInt(style.marginLeft) + x.scrollLeft + "px";
     }
+
     let el2 = document.getElementsByClassName("right_arrow")[0];
     let right = x.scrollWidth - (x.scrollLeft + x.clientWidth) + 1;
+    
+    el2.style.left="auto";
+
     if (right <= 2) {
       el2.style.display = "none";
-    } else {
+      el.style.left = (x.scrollLeft - el2.clientWidth) + "px";
+    } 
+    else {
       el2.style.display = "flex";
+      el2.style.left = (parseInt(style.marginLeft) + x.scrollLeft + x.clientWidth - el2.clientWidth) + "px";
     }
   }, 550);
 }
-
-getScrollVal();
-
-
-// const swiper = new Swiper(".swiper-container", {
-//   slidesPerView: 2,
-//   slidesPerGroup: 1,
-//   centeredSlides: true,
-//   loop: true,
-//   navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//       },
-//    breakpoints: {
-//     // when window width is >= 600px
-//     600: {
-//       slidesPerView: 3,
-//       slidesPerGroup: 3,
-//       spaceBetween: 5,
-//       centeredSlides: true
-      
-//     },
-//      // when window width is >= 900px
-//      900: {
-//       slidesPerView: 3,
-//       slidesPerGroup: 3,
-//       spaceBetween: 5,
-//        centeredSlides: false
-      
-//     },
-//     // when window width is >= 1200px
-//     1200: {
-//       slidesPerView: 4,
-//       slidesPerGroup: 4,
-//       spaceBetween: 5,
-//       centeredSlides: false
-//     },
-     
-//      // when window width is >= 1500px
-//      1500: {
-//        slidesPerView: 5,
-//        slidesPerGroup: 5,
-//        spaceBetween: 5,
-//        centeredSlides: false
-//      },
-     
-//      // when window width is >= 1800px
-//     1800: {
-//       slidesPerView: 5,
-//       slidesPerGroup: 5,
-//       spaceBetween: 5,
-//       centeredSlides: false
-//     }
-//   }
-// });
