91d54dc9de97c1c116c4c435558bafe38eb90b5e
jhy11 <hha0303@naver.com>
2022-04-14 16:46:13 +0900

    Change Media file storage to S3

--- config/settings.py
+++ config/settings.py
@@ -10,24 +10,56 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
+import os, json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# access secrets.json
+secret_file = os.path.join(BASE_DIR, 'config', 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_env_variable(key):
+    try:
+        return secrets[key]
+    except KeyError:
+        error_msg = f"Set the {key} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+
+# access key and secrete access key for aws
+AWS_ACCESS_KEY_ID = get_env_variable("JJJ_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("JJJ_AWS_SECRET_ACCESS_KEY")
+
+AWS_REGION = 'ap-northeast-2'
+
+AWS_STORAGE_BUCKET_NAME = 'jjjtttbucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$bert^v6xrw9sa&yxgfsvl@@t#+6&jzc4y0zgdb66524k7#t^#'
+SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# set storage for media files
+DEFAULT_FILE_STORAGE = 'config.asset_storage.MediaStorage' 
 
 # Application definition
 
@@ -42,6 +74,7 @@ INSTALLED_APPS = [
     'config',
     'product',
     'chat',
+    'storages',
 ]
 
 MIDDLEWARE = [
