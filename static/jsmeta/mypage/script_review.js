b6a4d97e4c189185fc559eb40f9a29508efd326f
jhy11 <hha0303@naver.com>
2022-04-17 23:24:12 +0900

    Add review (list, post, detail)

---
+++ static/jsmeta/mypage/script_review.js
@@ -0,0 +1,96 @@
+function setAttributes(el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+}
+  
+document.addEventListener("DOMContentLoaded", function(){
+  
+    let table_reviewProduct = $('#dataTableHover-reviewProduct').DataTable({
+      'destroy': true,
+      'autoWidth': false,
+  
+      'ajax': {
+        'type' : 'GET',
+        'url': '/mypage/review-table',
+        'dataSrc': 'reviewProduct'
+      },
+      columnDefs: [
+        {
+            "targets": 3,
+            "render": function (data) {
+                let td = document.createElement('td');
+                td.setAttribute('class', 'align-middle');
+                
+                let btn_review = document.createElement('button');
+                setAttributes(btn_review, {
+                    'type': 'button',
+                    'class': "btn btn-outline-primary pb-1",
+                    'onclick':"location.href='/mypage/review-post/"+data.id+"'"
+                });
+                
+                btn_review.innerText = '리뷰작성';
+                td.appendChild(btn_review);
+                return td.innerHTML;
+            }
+        },],
+        
+      columns: [
+        {data : 'order__date'},
+        {data : 'product__shop__shop_name'},
+        {data : 'product__name'},
+        {data : null},
+      ],
+    });
+  
+    let table_review = $('#dataTableHover-review').DataTable({
+      'destroy': true,
+      'autoWidth': false,
+  
+      'ajax': {
+        'type' : 'GET',
+        'url': '/mypage/review-table',
+        'dataSrc': 'review'
+      },
+      createdRow: function( row, data, dataIndex ) {
+        $( row )
+            .attr('onclick', "window.open('/mypage/review-detail/"+data.id +"','_self')");
+      },
+      columns: [
+        {data : 'created_at'},
+        {data : 'product__shop__shop_name'},
+        {data : 'product__name'},
+        {data : 'rate'},
+    ],
+});
+});
+
+
+
+
+//delete product
+/*
+async function reviewPost(id) {
+    const response = await fetch('review-post', {
+      method: 'POST',
+      headers: new Headers({
+        'X-CSRFToken': getCookie('csrftoken'),
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        Id: id,
+      }),
+    }).catch((error) => {
+      alert(error);
+    });
+  
+    const result = await response.json();
+  
+    if(result.success){
+        alert("상품이 삭제되었습니다");
+        location.href='/management/product';
+    }else{
+        alert("error");
+    }
+}
+*/
---
+++ static/jsmeta/mypage/script_review_post.js
@@ -0,0 +1,57 @@
+const btnSubmit = document.getElementById('btnSubmit');
+
+
+document.getElementById('mainImg').onchange = function () {
+  let file = $('#mainImg')[0].files[0];
+  if (file){
+    document.getElementById('mainImg-label').innerText=file.name;
+  }
+};
+
+
+
+let input = document.getElementById("mainImg"),
+preview = document.getElementById("preview");
+    
+input.addEventListener("change", function() {
+  changeImage(this);
+});
+
+function changeImage(input) {
+  let reader;
+
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+      preview.setAttribute('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+    
+    document.getElementById('mainImg-label').innerText=input.files[0].name;
+  }
+}
+
+btnSubmit.addEventListener('click', async() => {
+    const formData = new FormData(document.getElementById('uploadImgForm'));
+    formData.append('rate', document.getElementById('rate').value);
+    formData.append('content', document.getElementById('content').value);
+    formData.append('proId', document.getElementById('proId').value);
+
+    console.log(content);
+   
+    const response = await fetch('review-post', {
+        method: 'POST',
+        headers: {'X-CSRFToken': getCookie('csrftoken')},
+        body: formData,
+    })
+    .catch((error) => {
+        alert(error);
+    })
+
+    const result = await response.json()
+    if (result.success){
+        alert("리뷰가 등록되었습니다.");
+        location.href='/mypage/review';
+    }
+})
